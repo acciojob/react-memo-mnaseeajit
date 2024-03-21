0a10d21963075f61723bf579df68989af71ce559
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2024-03-21 12:43:56 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,25 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+//<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React from "react";
+import Usememo from "./useMemo";
+import Reactmemo from "./ReactMemo";
+import { useState } from "react";
+
+const App = () => {
+    const[count , setCount] = useState(0)
+
+    const increaseCount = () => {
+        setCount(count + 1);
+    }
+    return (
+        <div>
+            <h1>Task Manager</h1>
+             <button onClick={increaseCount}>Increment Counter</button>
+             <p>Count : {count}</p>
+            <Usememo />
+            <Reactmemo />
+        </div>
+    )
+}
+
+export default App;
